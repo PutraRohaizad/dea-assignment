7fc7c9236582712cb02535d15dffbe0d6e928a95
PutraRohaizad <putrarohayzad@gmail.com>
2019-05-17 09:47:07 +0800

    latest

--- client/src/app/authentication.service.ts
+++ client/src/app/authentication.service.ts
@@ -6,7 +6,6 @@ import { Router } from '@angular/router'
 
 //declaration on types..
 export interface UserDetails {
-<<<<<<< HEAD
   // id: number
   // company_name: string
   // company_pic: string
@@ -43,20 +42,13 @@ export interface TokenPayload {
   // departments: any
 
   id: number
-<<<<<<< HEAD
   first_name: string
   last_name: string
   register_no: string
   password: string
   // exp: number
   // iat: number
-=======
-  company_name: string
-  company_pic: number
-  company_regno: string
-  password: string
-  address: string
->>>>>>> 419af5c1abb3694239527a2b76ab00703ec79443
+
 }
 
 @Injectable()
